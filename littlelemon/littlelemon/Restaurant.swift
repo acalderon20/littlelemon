0226a7ed16e0c954e796f037f46e61227b25a318
acalderon20 <acalderon20@coe.edu>
2023-06-18 16:42:22 -0500

    created a new branch to start the initial setup for the app

---
+++ littlelemon/littlelemon/Restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Adolfo Calderon on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
